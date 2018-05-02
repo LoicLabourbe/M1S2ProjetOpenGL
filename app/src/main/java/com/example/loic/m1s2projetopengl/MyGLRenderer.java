822b48c1b3ccd9ee113066f0e5297b460fe141c8
Loic Labourbe <loic.labourbe@etu.univ-orleans.fr>
2018-05-02 10:03:49 +0200

    cube

--- app/src/main/java/com/example/loic/m1s2projetopengl/MyGLRenderer.java
+++ app/src/main/java/com/example/loic/m1s2projetopengl/MyGLRenderer.java
@@ -3,7 +3,9 @@ package com.example.loic.m1s2projetopengl;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.os.SystemClock;
 
+import com.example.loic.m1s2projetopengl.Drawable.Cube;
 import com.example.loic.m1s2projetopengl.Drawable.Triangle;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -12,12 +14,13 @@ import javax.microedition.khronos.opengles.GL10;
 public class MyGLRenderer implements GLSurfaceView.Renderer{
 
     private Triangle mTriangle;
+    private Cube mCube;
 
 
     private final float[] mMVPMatrix = new float[16];
     private final float[] mProjectionMatrix = new float[16];
     private final float[] mViewMatrix = new float[16];
-
+    private float[] mRotationMatrix = new float[16];
 
     @Override
     public void onSurfaceCreated(GL10 unused, EGLConfig config) {
@@ -28,22 +31,42 @@ public class MyGLRenderer implements GLSurfaceView.Renderer{
         //initialize a triangle
         mTriangle = new Triangle();
 
-
+        mCube = new Cube();
     }
 
     @Override
     public void onDrawFrame(GL10 unused) {
+
+        float[] scratch = new float[16];
+
         //Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
         //set the camera position (View Matrix)
-        Matrix.setLookAtM(mViewMatrix,0,0,0,-3,0f,0f,0f,0f,1f,0f);
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, 5, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
 
         //Calculate the projection and view transformation
         Matrix.multiplyMM(mMVPMatrix, 0,mProjectionMatrix,0,mViewMatrix,0);
 
-        //draw shape
-        mTriangle.draw(mMVPMatrix);
+        // Create a rotation transformation for the triangle
+        long time = SystemClock.uptimeMillis() % 4000L;
+        float angle = 0.090f * ((int) time);
+        Matrix.setRotateM(mRotationMatrix, 0, angle, 0, -1.0f, 0f);
+
+        // Combine the rotation matrix with the projection and camera view
+        // Note that the mMVPMatrix factor *must be first* in order
+        // for the matrix multiplication product to be correct.
+
+        Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+
+
+        //draw triangle shape
+        //mTriangle.draw(scratch);
+
+        mCube.draw(scratch);
+
+
+
     }
 
     @Override
